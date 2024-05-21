92597b26fcf2d98a99452507c9b538a002ec6942
Sidney Shafer <sidney@mnshafers.com>
2024-05-21 15:57:02 -0500

    Create ReturnSum File and Add returnSum Function Code

---
+++ source-code/ReturnSum.js
@@ -0,0 +1,9 @@
+// ===============================================================
+// Write a function which takes two numbers and returns their sum.
+// ===============================================================
+
+function returnSum(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(returnSum(2, 3));
