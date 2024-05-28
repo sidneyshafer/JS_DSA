f4db1a3191f9fcf341bbb533ad964d04b1eb6c5c
Sidney Shafer <sidney@mnshafers.com>
2024-05-28 06:17:41 -0500

    Create Exercise 4

---
+++ source-code/Exercise_4.js
@@ -0,0 +1,29 @@
+// Write a function called isSubsequence which takes in two strings and checks whether the characters in the first string form a subsequence of the characters in the second string. In other words, the function should check whether the characters in the first string appear somewhere in the second string, without their order changing.
+
+// Example Output:
+
+// isSubsequence('hello', 'hello world'); // true
+// isSubsequence('sing', 'sting'); // true
+// isSubsequence('abc', 'abracadabra'); // true
+// isSubsequence('abc', 'acb'); // false (order matters)
+
+function isSubsequence(sub, str) {
+  let i = 0;
+  let j = 0;
+
+  while (j < str.length) {
+    if (sub[i] === str[j]) {
+      i++;
+    }
+    if (i === sub.length) {
+      return true;
+    }
+    j++;
+  }
+  return false;
+}
+
+console.log(isSubsequence('hello', 'hello world'));
+console.log(isSubsequence('sing', 'sting'));
+console.log(isSubsequence('abc', 'abracadabra'));
+console.log(isSubsequence('abc', 'acb'));
