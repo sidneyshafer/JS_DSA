d252fb48de6d225913634215cabfa7870cd4d6be
Sidney Shafer <sidney@mnshafers.com>
2024-06-24 05:46:19 -0500

    Add Doubly Linked List Methods to Class

--- source-code/Doubly_Linked_List.js
+++ source-code/Doubly_Linked_List.js
@@ -13,4 +13,116 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+  //PUSH METHOD: add a node to the end of a doubly linked list
+  push(val) {
+    var newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  //POP METHOD: remove a node from the end of a doubly linked list
+  pop() {
+    if (!this.head) return undefined;
+    
+    var poppedNode = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = poppedNode.prev;
+      this.tail.next = null;
+      poppedNode.prev = null;
+    }
+    this.length--;
+    return poppedNode;
+  }
+  //SHIFT METHOD: remove a node from the beginning of a doubly linked list
+  shift() {
+    if (this.length === 0) return undefined;
+
+    var oldHead = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = oldHead.next;
+      this.head.prev = null;
+      oldHead.next = null;
+    }
+    this.length--;
+    return oldHead;
+  }
+  //UNSHIFT METHOD: add a node to the beginning of a doubly linked list
+  unshift(val) {
+    var newNode = new Node(val);
+
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.head.prev = newNode;
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  //GET METHOD: access a node in a doubly linked list by its position
+  get(idx) {
+    if (idx < 0 || idx >= this.length) return null;
+
+    var count, curr;
+
+    if (idx <= this.length/2) {
+      count = 0;
+      curr = this.head;
+
+      while (count !== idx) {
+        curr = curr.next;
+        count++;
+      }
+    } else {
+      count = this.length-1;
+      curr = this.tail;
+
+      while (count !== idx) {
+        curr = curr.prev;
+        count--;
+      }
+    }
+    return curr;
+  }
+  //SET METHOD: replacing the value of a node in a doubly linked list
+  set(idx, val) {
+    var foundNode = this.get(idx);
+    if (foundNode) {
+      foundNode.val = val;
+      return true;
+    }
+    return false;
+  }
 }
+
+var list = new DoublyLinkedList();
+
+list.push(100);
+list.push(200);
+list.push(300);
+list.push(400);
+list.push(500);
+list.push(600);
+// console.log(list);
+// list.pop();
+// console.log(list.shift());
+console.log(list);
+// console.log(list.set(1, 50));
+// console.log(list.get(0));
+// console.log(list.unshift(100));
+// console.log(list.shift());
