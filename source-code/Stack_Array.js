e3983f359d35d94a8b4e03d35e227eb6f7c25d9f
Sidney Shafer <sidney@mnshafers.com>
2024-06-25 07:06:18 -0500

    Create Stack from Array Example Code

---
+++ source-code/Stack_Array.js
@@ -0,0 +1,11 @@
+var stack = [];
+
+stack.push("google");
+stack.push("instagram");
+stack.push("youtube");
+
+// console.log(stack.pop());
+stack.unshift("amazon");
+
+console.log(stack.shift());
+console.log(stack);
