d0473e423965f0ff214eb30f39b2dd86121cc082
Sidney Shafer <sidney@mnshafers.com>
2024-07-11 06:36:11 -0500

    Add Method for Adding an Edge

--- source-code/Graph.js
+++ source-code/Graph.js
@@ -5,4 +5,18 @@ class Graph {
     addVertex(vertex) {
         if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
     }
+    addEdge(v1, v2) {
+        this.adjacencyList[v1].push(v2);
+        this.adjacencyList[v2].push(v1);
+    }
 }
+
+var g = new Graph();
+g.addVertex("Apple");
+g.addVertex("Bread");
+g.addVertex("Eggs");
+
+g.addEdge("Apple", "Bread");
+g.addEdge("Eggs", "Bread");
+
+console.log(g);
