8450a862780b8d960ca8172c53473fa34f57bb6d
Sidney Shafer <sidney@mnshafers.com>
2024-07-11 06:23:06 -0500

    Create addVertex Method

--- source-code/Graph.js
+++ source-code/Graph.js
@@ -2,4 +2,7 @@ class Graph {
     constructor() {
         this.adjacencyList = {};
     }
+    addVertex(vertex) {
+        if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+    }
 }
