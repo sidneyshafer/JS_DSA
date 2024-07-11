3ce4c59a65434c4acff97c5b773b811705de2775
Sidney Shafer <sidney@mnshafers.com>
2024-07-11 06:43:13 -0500

    Add Method for Removing an Edge

--- source-code/Graph.js
+++ source-code/Graph.js
@@ -9,6 +9,14 @@ class Graph {
         this.adjacencyList[v1].push(v2);
         this.adjacencyList[v2].push(v1);
     }
+    removeEdge(v1, v2) {
+        this.adjacencyList[v1] = this.adjacencyList[v1].filter(
+            v => v !== v2
+        );
+        this.adjacencyList[v2] = this.adjacencyList[v2].filter(
+            v => v !== v1
+        );
+    }
 }
 
 var g = new Graph();
@@ -18,5 +26,6 @@ g.addVertex("Eggs");
 
 g.addEdge("Apple", "Bread");
 g.addEdge("Eggs", "Bread");
+// g.removeEdge("Eggs", "Bread");
 
 console.log(g);
